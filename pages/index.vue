835bf3544bcd45fdc66488b58f064365762249ff
monjuAflame <monjuaflame@gmail.com>
2021-08-31 14:09:18 +0600

    axios install and first api call

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,31 @@
 <template>
+  <div>
+    <h1 v-if="apiValue">Check: {{ apiValue }}</h1>
+    <button @click.prevent="getBackendRequest()" >click</button>
   <Tutorial/>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data(){
+    return {
+      apiValue: ''
+    }
+  },
+
+  mounted(){
+    // this.getBackendRequest();
+  },
+
+  methods: {
+    getBackendRequest(){
+      this.$axios.get('test').then(res => {
+        this.apiValue = res.data;
+      })
+    }
+  }
+
+  
+}
 </script>
